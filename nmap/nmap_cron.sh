1c63ec06a2faf5c43042bc26ded44fa345ca7de3
mvoseismic <mvoseismic@gmail.com>
2025-08-01 17:52:53 +0000

    Fixed bug in nmap_cron.sh.

--- nmap/nmap_cron.sh
+++ nmap/nmap_cron.sh
@@ -5,4 +5,4 @@
 #
 # R.C. Stewart, 2021-08-06
 
-/usr/bin/nmap 172.17.102.0-126 -oX - | /usr/bin/xsltproc -html - > /home/seisan/data/statusMonitoring/nmap/nmap.html
+/usr/bin/nmap 172.17.102.0-126 -oX - | /usr/bin/xsltproc -html - > /home/seisan/data/statusMonitoring/network/nmap/nmap.html
